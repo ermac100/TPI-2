7e68f468a1b06a61538e991c1abba11f1555ffb9
Rodrigo Leite <rodrigoleitelira@Gmail.com>
2018-08-20 20:22:00 -0300

    aula

---
+++ init.sh
@@ -0,0 +1,5 @@
+git clone git remote add origin https://github.com/ermac100/tpi-ii.git
+git config --global user.email "rodrigoleitelira@Gmail.com "
+git config --global user.name "Rodrigo Leite"
+cd tpi-ii
+code .
